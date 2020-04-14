a9f50a638e4d4cd69cb31e2ae72d31739dc05487
Ryan Peterson <ryanjpeterson93@gmail.com>
2020-04-14 06:34:19 -0600

    built out dummy components for form/styles/app and completed Item controller

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,2 +1,25 @@
 class Api::ItemsController < ApplicationController
+  
+  def index
+    render json: Item.all
+  end
+
+  def show
+    render json: Item.find(params[:id])
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: item
+    else 
+      render json: {message: 'Item Created'}
+    end
+  end
+
+private
+    
+  def item_params
+    params.require(:item).permit(:name, :image, :description, :likes)
+  end
 end
---
+++ client/src/components/ItemForm.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class ItemForm extends React.Component {
+  render() {
+    return (
+      <div>
+        This is a form
+      </div>
+    );
+  }
+}
+
+export default ItemForm
